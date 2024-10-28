800bb8c8a6431f26d1ae00dbf0edfaa407d5b05e
congit27 <conbo1102a@gmail.com>
2024-10-28 19:25:56 +0700

    feat: cv nanalyzer with custom model

--- AI-services/scripts/cv-analyzer/App.py
+++ AI-services/scripts/cv-analyzer/App.py
@@ -1,41 +1,32 @@
-import streamlit as st
-import pandas as pd
-import base64,random
-import time,datetime
-#libraries to parse the resume pdf files
-from pyresparser import ResumeParser
-from pdfminer3.layout import LAParams, LTTextBox
-from pdfminer3.pdfpage import PDFPage
-from pdfminer3.pdfinterp import PDFResourceManager
-from pdfminer3.pdfinterp import PDFPageInterpreter
-from pdfminer3.converter import TextConverter
-import io,random
-from streamlit_tags import st_tags
-from PIL import Image
-import nltk
-nltk.download('stopwords')
+import sys, fitz
+import spacy
+import csv
 
+fname = 'Uploaded_Resumes/CV2.pdf'
+doc = fitz.open(fname)
 
-def pdf_reader(file):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-    with open(file, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-            print(page)
-        text = fake_file_handle.getvalue()
+# doc = [page for page in doc]
+# print(doc)
 
-    # close open handles
-    converter.close()
-    fake_file_handle.close()
-    return text
+text = ""
+for page in doc:
+    text = text + str(page.get_text())
 
+text = text.strip()
+text = ' '.join(text.split())
 
-save_image_path = r'D:\Workspace\job-cv-ai\AI-services\models\cv-analyser\Uploaded_Resumes\IOS1.pdf'
-resume_data = ResumeParser(save_image_path).get_extracted_data()
+model_path = "D:\Workspace\job-cv-ai\AI-services\models\cv-parser\model-best"
+nlp = spacy.load(model_path)
+doc = nlp(text)
 
-print(resume_data)
+with open("extracted_cv_data.csv", mode="w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+
+    # Ghi header cho file CSV
+    writer.writerow(["Entity", "Label"])
+
+    # Ghi từng entity và nhãn của nó vào file CSV
+    for ent in doc.ents:
+        writer.writerow([ent.text, ent.label_])
+
+print("Data has been saved to extracted_cv_data.csv")
